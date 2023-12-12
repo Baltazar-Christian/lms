a37a8e4a7c3033322692124324e59e58c68d7544
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-12-12 22:38:44 +0300

    new modifications

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -27,60 +27,16 @@
 
     <!-- Stylesheets -->
     <!-- OneUI framework -->
-    <link rel="stylesheet" id="css-main" href="assets/css/oneui.min.css">
+    <link rel="stylesheet" id="css-main" href="{{ asset('assets/css/oneui.min.css')}}">
 
     <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
-    <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/amethyst.min.css"> -->
+    <link rel="stylesheet" id="css-theme" href="assets/css/themes/amethyst.min.css"> 
     <!-- END Stylesheets -->
   </head>
 
   <body>
     <!-- Page Container -->
-    <!--
-      Available classes for #page-container:
-
-      GENERIC
-
-        'remember-theme'                            Remembers active color theme and dark mode between pages using localStorage when set through
-                                                    - Theme helper buttons [data-toggle="theme"],
-                                                    - Layout helper buttons [data-toggle="layout" data-action="dark_mode_[on/off/toggle]"]
-                                                    - ..and/or One.layout('dark_mode_[on/off/toggle]')
-
-      SIDEBAR & SIDE OVERLAY
-
-        'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
-        'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
-        'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
-        'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
-        'sidebar-dark'                              Dark themed sidebar
-
-        'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
-        'side-overlay-o'                            Visible Side Overlay by default
-
-        'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
-
-        'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
-
-      HEADER
-
-        ''                                          Static Header if no class is added
-        'page-header-fixed'                         Fixed Header
-
-      HEADER STYLE
-
-        ''                                          Light themed Header
-        'page-header-dark'                          Dark themed Header
-
-      MAIN CONTENT LAYOUT
-
-        ''                                          Full width Main Content if no class is added
-        'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
-        'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
-
-      DARK MODE
-
-        'sidebar-dark page-header-dark dark-mode'   Enable dark mode (light sidebar/header is not supported with dark mode)
-    -->
+   
     <div id="page-container" class="sidebar-o sidebar-dark enable-page-overlay side-scroll page-header-fixed main-content-narrow">
   
 
@@ -103,10 +59,10 @@
         <div class="content py-3">
           <div class="row fs-sm">
             <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-end">
-              Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="https://1.envato.market/ydb" target="_blank">pixelcave</a>
+              {{-- Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="https://1.envato.market/ydb" target="_blank">pixelcave</a> --}}
             </div>
             <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-start">
-              <a class="fw-semibold" href="https://1.envato.market/AVD6j" target="_blank">OneUI 5.7</a> &copy; <span data-toggle="year-copy"></span>
+              <a class="fw-semibold" href="https://1.envato.market/AVD6j" target="_blank">TazarChriss</a> &copy; <span data-toggle="year-copy"></span>
             </div>
           </div>
         </div>
@@ -115,18 +71,13 @@
     </div>
     <!-- END Page Container -->
 
-    <!--
-        OneUI JS
 
-        Core libraries and functionality
-        webpack is putting everything together at assets/_js/main/app.js
-    -->
-    <script src="assets/js/oneui.app.min.js"></script>
+    <script src="{{ asset('assets/js/oneui.app.min.js') }}"></script>
 
     <!-- Page JS Plugins -->
-    <script src="assets/js/plugins/chart.js/chart.umd.js"></script>
+    <script src="{{ asset('assets/js/plugins/chart.js/chart.umd.js') }}"></script>
 
     <!-- Page JS Code -->
-    <script src="assets/js/pages/be_pages_dashboard.min.js"></script>
+    <script src="{{ asset('assets/js/pages/be_pages_dashboard.min.js') }}"></script>
   </body>
 </html>
