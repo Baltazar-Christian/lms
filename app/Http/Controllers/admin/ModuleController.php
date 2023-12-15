c8be725153aa26f29a8cad634a0c12ec46e4c6b4
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-12-15 06:38:10 +0300

    created modules views

---
+++ app/Http/Controllers/admin/ModuleController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Models\Module;
+use App\Models\Instute;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class ModuleController extends Controller
+{
+    public function index()
+    {
+        $modules = Module::all();
+        return view('admin.modules.index', compact('modules'));
+    }
+
+    public function create()
+    {
+        $institutes = Instute::all();
+        return view('admin.modules.create', compact('institutes'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:modules',
+            'institutes' => 'required|array',
+            'institutes.*' => 'exists:instutes,id',
+        ]);
+
+        $moduleData = [
+            'name' => $request->input('name'),
+            'status' => 'active',
+            'created_by' => Auth::id(),
+        ];
+
+        $module = Module::create($moduleData);
+
+        // Attach institutes to the module
+        $module->institutes()->attach($request->input('institutes'), [
+            'status' => 'active',
+            'created_by' => Auth::id(),
+        ]);
+
+        return redirect()->route('admin.modules.index')->with('success', 'Module created successfully');
+    }
+
+    public function edit($id)
+    {
+        $module = Module::findOrFail($id);
+        $institutes = Instute::all();
+        return view('admin.modules.edit', compact('module', 'institutes'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|unique:modules,name,' . $id,
+            'institutes' => 'required|array',
+            'institutes.*' => 'exists:instutes,id',
+        ]);
+
+        $moduleData = [
+            'name' => $request->input('name'),
+            'status' => 'active', // You can adjust this based on your logic
+            'created_by' => Auth::id(),
+        ];
+
+        $module = Module::findOrFail($id);
+        $module->update($moduleData);
+
+        // Sync institutes for the module
+        $module->institutes()->sync($request->input('institutes'), [
+            'status' => 'active',
+            'created_by' => Auth::id(),
+        ]);
+
+        return redirect()->route('admin.modules.index')->with('success', 'Module updated successfully');
+    }
+
+    public function destroy($id)
+    {
+        $module = Module::findOrFail($id);
+        $module->institutes()->detach(); // Detach institutes before deleting the module
+        $module->delete();
+
+        return redirect()->route('admin.modules.index')->with('success', 'Module deleted successfully');
+    }
+}
