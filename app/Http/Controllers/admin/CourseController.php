81c599657fa90163bea746d4767b828db3a354db
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-12-15 16:31:57 +0300

    added course controller

--- app/Http/Controllers/admin/CourseController.php
+++ app/Http/Controllers/admin/CourseController.php
@@ -2,10 +2,105 @@
 
 namespace App\Http\Controllers\admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Course;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CourseController extends Controller
 {
-    //
+    public function index()
+    {
+        $courses = Course::all();
+        return view('courses.index', compact('courses'));
+    }
+
+
+
+    public function create()
+    {
+        return view('courses.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'slug' => 'required|string|unique:courses',
+            'price' => 'required|numeric',
+            'duration_in_minutes' => 'required|integer',
+            'is_published' => 'boolean',
+            'published_at' => 'nullable|date',
+            'cover_image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        // Handle cover image upload and storage
+        if ($request->hasFile('cover_image')) {
+            $coverImage = $request->file('cover_image');
+            $imageName = time() . '.' . $coverImage->getClientOriginalExtension();
+            $coverImage->storeAs('covers', $imageName, 'public'); // Adjust the storage path as needed
+
+            $request->merge(['cover_image' => $imageName]);
+        }
+
+        Course::create($request->all());
+
+        return redirect()->route('courses.index')->with('success', 'Course created successfully.');
+    }
+
+    public function show(Course $course)
+    {
+        return view('courses.show', compact('course'));
+    }
+
+    public function edit(Course $course)
+    {
+        return view('courses.edit', compact('course'));
+    }
+
+    public function update(Request $request, Course $course)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'slug' => 'required|string|unique:courses,slug,' . $course->id,
+            'price' => 'required|numeric',
+            'duration_in_minutes' => 'required|integer',
+            'is_published' => 'boolean',
+            'published_at' => 'nullable|date',
+            'cover_image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        // Handle cover image update
+        if ($request->hasFile('cover_image')) {
+            // Delete the old cover image if exists
+            if ($course->cover_image) {
+                Storage::disk('public')->delete('covers/' . $course->cover_image);
+            }
+
+            // Upload the new cover image
+            $coverImage = $request->file('cover_image');
+            $imageName = time() . '.' . $coverImage->getClientOriginalExtension();
+            $coverImage->storeAs('covers', $imageName, 'public'); // Adjust the storage path as needed
+
+            $request->merge(['cover_image' => $imageName]);
+        }
+
+        $course->update($request->all());
+
+        return redirect()->route('courses.index')->with('success', 'Course updated successfully.');
+    }
+
+    public function destroy(Course $course)
+    {
+        // Delete the cover image
+        if ($course->cover_image) {
+            Storage::disk('public')->delete('covers/' . $course->cover_image);
+        }
+
+        $course->delete();
+
+        return redirect()->route('courses.index')->with('success', 'Course deleted successfully.');
+    }
 }
+
