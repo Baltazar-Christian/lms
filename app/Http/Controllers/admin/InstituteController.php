a7ae951e168058e6360ea545398e1913b1a70133
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-12-15 04:09:22 +0300

    fixed controller

--- app/Http/Controllers/admin/InstituteController.php
+++ app/Http/Controllers/admin/InstituteController.php
@@ -23,13 +23,35 @@ class InstituteController extends Controller
     {
         $request->validate([
             'name' => 'required',
+            'logo' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Adjust the validation rules as needed
+            'description' => 'nullable|string',
+            'contact_address' => 'nullable|string',
+            'contact_phone' => 'nullable|string',
+            'contact_email' => 'nullable|email',
+            'website' => 'nullable|url',
             'code' => 'required|unique:institutes',
-            // Add more validation rules as needed
+            'status' => 'required|in:active,blocked',
         ]);
 
-        Instute::create($request->all());
+        // Handle file upload
+        if ($request->hasFile('logo')) {
+            $logoPath = $request->file('logo')->store('logos', 'public');
+        }
 
-        return redirect()->route('admin.institutes.index')->with('success', 'Institute created successfully');
+        // Create the institute
+        Instute::create([
+            'name' => $request->input('name'),
+            'logo' => $logoPath ?? null,
+            'description' => $request->input('description'),
+            'contact_address' => $request->input('contact_address'),
+            'contact_phone' => $request->input('contact_phone'),
+            'contact_email' => $request->input('contact_email'),
+            'website' => $request->input('website'),
+            'code' => $request->input('code'),
+            'status' => $request->input('status'),
+        ]);
+
+        return redirect()->route('institutes.index')->with('success', 'Institute created successfully');
     }
 
     public function edit($id)
@@ -42,14 +64,43 @@ class InstituteController extends Controller
     {
         $request->validate([
             'name' => 'required',
+            'logo' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Adjust the validation rules as needed
+            'description' => 'nullable|string',
+            'contact_address' => 'nullable|string',
+            'contact_phone' => 'nullable|string',
+            'contact_email' => 'nullable|email',
+            'website' => 'nullable|url',
             'code' => 'required|unique:institutes,code,' . $id,
-            // Add more validation rules as needed
+            'status' => 'required|in:active,blocked',
         ]);
 
-        $institute = Instute::findOrFail($id);
-        $institute->update($request->all());
+        $institute = Institute::findOrFail($id);
 
-        return redirect()->route('admin.institutes.index')->with('success', 'Institute updated successfully');
+        // Handle file upload
+        if ($request->hasFile('logo')) {
+            // Delete the previous logo if it exists
+            if ($institute->logo) {
+                Storage::disk('public')->delete($institute->logo);
+            }
+
+            $logoPath = $request->file('logo')->store('logos', 'public');
+        }
+
+        // Update the institute
+        $institute->update([
+            'name' => $request->input('name'),
+            'logo' => $logoPath ?? $institute->logo,
+            'description' => $request->input('description'),
+            'contact_address' => $request->input('contact_address'),
+            'contact_phone' => $request->input('contact_phone'),
+            'contact_email' => $request->input('contact_email'),
+            'website' => $request->input('website'),
+            'code' => $request->input('code'),
+            'status' => $request->input('status'),
+            // Add other fields
+        ]);
+
+        return redirect()->route('institutes.index')->with('success', 'Institute updated successfully');
     }
 
     public function destroy($id)
@@ -57,6 +108,6 @@ class InstituteController extends Controller
         $institute = Instute::findOrFail($id);
         $institute->delete();
 
-        return redirect()->route('admin.institutes.index')->with('success', 'Institute deleted successfully');
+        return redirect()->route('institutes.index')->with('success', 'Institute deleted successfully');
     }
 }
