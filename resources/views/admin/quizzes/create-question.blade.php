8795ce4e23a3d59a645245c9b0c0da4329fcddc9
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-12-24 02:58:28 +0300

    minor view fixes

--- resources/views/admin/quizzes/create-question.blade.php
+++ resources/views/admin/quizzes/create-question.blade.php
@@ -1,16 +1,23 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Create Question</h1>
+    <div class="container mt-4">
+        <h1>Create Question</h1>
 
-    <form action="{{ route('lms.store-question', [$course->id, $quiz->id]) }}" method="post">
-        @csrf
+        <div class="card mt-3">
+            <div class="card-body">
+                <form action="{{ route('lms.store-question', [$course->id, $quiz->id]) }}" method="post">
+                    @csrf
 
-        <div class="form-group">
-            <label for="text">Question Text</label>
-            <input type="text" name="text" id="text" class="form-control" required>
+                    <div class="form-group">
+                        <label for="text">Question Text</label>
+                        <input type="text" name="text" id="text" class="form-control" required>
+                    </div>
+
+                    <br>
+                    <button type="submit" class="btn btn-primary">Add Question</button>
+                </form>
+            </div>
         </div>
-
-        <button type="submit" class="btn btn-primary">Add Question</button>
-    </form>
+    </div>
 @endsection
--- resources/views/admin/quizzes/show-question.blade.php
+++ resources/views/admin/quizzes/show-question.blade.php
@@ -1,35 +1,43 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>Question Details</h1>
+    <div class="container mt-4">
+        <h1>Question Details</h1>
 
-    <p><strong>ID:</strong> {{ $question->id }}</p>
-    <p><strong>Text:</strong> {{ $question->text }}</p>
+        <div class="card mt-3">
+            <div class="card-body">
+                <p class="card-text"><strong>ID:</strong> {{ $question->id }}</p>
+                <p class="card-text"><strong>Text:</strong> {{ $question->text }}</p>
+            </div>
+        </div>
 
-    <h2>Answers</h2>
+        <div class="mt-4">
+            <h2>Answers</h2>
 
-    <a href="{{ route('lms.create-answer', [$course->id, $quiz->id, $question->id]) }}" class="btn btn-primary">Create Answer</a>
+            <a href="{{ route('lms.create-answer', [$course->id, $quiz->id, $question->id]) }}" class="btn btn-primary float-end">Create Answer</a>
 
-    <table class="table mt-3">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Text</th>
-                <th>Is Correct</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($question->answers as $answer)
-                <tr>
-                    <td>{{ $answer->id }}</td>
-                    <td>{{ $answer->text }}</td>
-                    <td>{{ $answer->is_correct ? 'Yes' : 'No' }}</td>
-                    <td>
-                        <a href="{{ route('lms.show-answer', [$course->id, $quiz->id, $question->id, $answer->id]) }}" class="btn btn-sm btn-info">View</a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+            <table class="table mt-3">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Text</th>
+                        <th>Is Correct</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($question->answers as $answer)
+                        <tr>
+                            <td>{{ $answer->id }}</td>
+                            <td>{{ $answer->text }}</td>
+                            <td>{{ $answer->is_correct ? 'Yes' : 'No' }}</td>
+                            <td>
+                                <a href="{{ route('lms.show-answer', [$course->id, $quiz->id, $question->id, $answer->id]) }}" class="btn btn-sm btn-info">View</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
