a82225fee8d398f8dda340768ac01d985fbc5724
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-12-25 17:12:14 +0300

    fixed

--- app/Http/Controllers/admin/AdminPasswordResetController.php
+++ app/Http/Controllers/admin/AdminPasswordResetController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers\admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AdminPasswordResetController extends Controller
 {
-    //
+    public function showResetForm(User $user)
+    {
+        return view('admin.password.reset', ['user' => $user]);
+    }
+
+    public function reset(Request $request, User $user)
+    {
+        $request->validate([
+            'password' => 'required|confirmed|min:8',
+        ]);
+
+        $status = Password::broker()->reset(
+            ['email' => $user->email, 'token' => $request->token, 'password' => $request->password]
+        );
+
+        return $status == Password::PASSWORD_RESET
+            ? redirect()->route('admin.dashboard')->with('status', __($status))
+            : back()->withErrors(['email' => __($status)]);
+    }
 }
