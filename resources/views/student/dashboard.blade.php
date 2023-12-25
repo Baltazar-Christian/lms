7237516dbd936d28ee3f5dbe37dcbc787bf4fb3e
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-12-26 02:09:00 +0300

    student dashboard modifications

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -85,56 +85,10 @@
                       </div>
                     </a>
                   </li>
-                  <li>
-                    <a class="text-dark d-flex py-2" href="javascript:void(0)">
-                      <div class="flex-shrink-0 me-2 ms-3">
-                        <i class="fa fa-fw fa-times-circle text-danger"></i>
-                      </div>
-                      <div class="flex-grow-1 pe-2">
-                        <div class="fw-semibold">Update failed, restart server</div>
-                        <span class="fw-medium text-muted">26 min ago</span>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <a class="text-dark d-flex py-2" href="javascript:void(0)">
-                      <div class="flex-shrink-0 me-2 ms-3">
-                        <i class="fa fa-fw fa-plus-circle text-primary"></i>
-                      </div>
-                      <div class="flex-grow-1 pe-2">
-                        <div class="fw-semibold">2 new sales, keep it up</div>
-                        <span class="fw-medium text-muted">33 min ago</span>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <a class="text-dark d-flex py-2" href="javascript:void(0)">
-                      <div class="flex-shrink-0 me-2 ms-3">
-                        <i class="fa fa-fw fa-user-plus text-success"></i>
-                      </div>
-                      <div class="flex-grow-1 pe-2">
-                        <div class="fw-semibold">You have a new subscriber</div>
-                        <span class="fw-medium text-muted">41 min ago</span>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <a class="text-dark d-flex py-2" href="javascript:void(0)">
-                      <div class="flex-shrink-0 me-2 ms-3">
-                        <i class="fa fa-fw fa-check-circle text-success"></i>
-                      </div>
-                      <div class="flex-grow-1 pe-2">
-                        <div class="fw-semibold">You have a new follower</div>
-                        <span class="fw-medium text-muted">42 min ago</span>
-                      </div>
-                    </a>
-                  </li>
+
+
                 </ul>
-                <div class="p-2 border-top text-center">
-                  <a class="d-inline-block fw-medium" href="javascript:void(0)">
-                    <i class="fa fa-fw fa-arrow-down me-1 opacity-50"></i> Load More..
-                  </a>
-                </div>
+
               </div>
             </div>
             <!-- END Notifications Dropdown -->
@@ -164,15 +118,15 @@
             <!-- User Dropdown -->
             <div class="dropdown d-inline-block ms-2">
               <button type="button" class="btn btn-sm btn-alt-secondary d-flex align-items-center" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img class="rounded-circle" src="assets/media/avatars/avatar10.jpg" alt="Header Avatar" style="width: 21px;" />
-                <span class="d-none d-sm-inline-block ms-2">John</span>
+                <img class="rounded-circle" src="{{ asset('assets/media/avatars/avatar10.jpg') }}" alt="Header Avatar" style="width: 21px;" />
+                <span class="d-none d-sm-inline-block ms-2">{{ Auth::user()->name }}</span>
                 <i class="fa fa-fw fa-angle-down d-none d-sm-inline-block opacity-50 ms-1"></i>
               </button>
               <div class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0" aria-labelledby="page-header-user-dropdown">
                 <div class="p-3 text-center bg-body-light border-bottom rounded-top">
                   <img class="img-avatar img-avatar48 img-avatar-thumb" src="assets/media/avatars/avatar10.jpg" alt="">
-                  <p class="mt-2 mb-0 fw-medium">John Smith</p>
-                  <p class="mb-0 text-muted fs-sm fw-medium">Web Developer</p>
+                  <p class="mt-2 mb-0 fw-medium">{{ Auth::user()->name }}</p>
+                  <p class="mb-0 text-muted fs-sm fw-medium"> Student </p>
                 </div>
                 <div class="p-2">
                   <a class="dropdown-item d-flex align-items-center justify-content-between" href="be_pages_generic_inbox.html">
@@ -225,16 +179,7 @@
         </div>
         <!-- END Header Search -->
 
-        <!-- Header Loader -->
-        <!-- Please check out the Loaders page under Components category to see examples of showing/hiding it -->
-        <div id="page-header-loader" class="overlay-header bg-primary-lighter">
-          <div class="content-header">
-            <div class="w-100 text-center">
-              <i class="fa fa-fw fa-circle-notch fa-spin text-primary"></i>
-            </div>
-          </div>
-        </div>
-        <!-- END Header Loader -->
+
       </header>
       <!-- END Header -->
 
@@ -258,16 +203,15 @@
               <div id="main-navigation" class="d-none d-lg-block mt-2 mt-lg-0">
                 <ul class="nav-main nav-main-dark nav-main-horizontal nav-main-hover">
                   <li class="nav-main-item">
-                    <a class="nav-main-link active" href="bd_dashboard.html">
+                    <a class="nav-main-link active" href="{{ route('lms.student-dashboard') }}">
                       <i class="nav-main-link-icon si si-compass"></i>
                       <span class="nav-main-link-name">Dashboard</span>
                     </a>
                   </li>
-                  <li class="nav-main-heading">Pages</li>
                   <li class="nav-main-item">
                     <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
-                      <i class="nav-main-link-icon si si-puzzle"></i>
-                      <span class="nav-main-link-name">Variations</span>
+                      <i class="nav-main-link-icon fa fa-book"></i>
+                      <span class="nav-main-link-name">Available Courses</span>
                     </a>
                     <ul class="nav-main-submenu">
                       <li class="nav-main-item">
@@ -327,60 +271,16 @@
                   <li class="nav-main-item">
                     <a class="nav-main-link" href="bd_search.html">
                       <i class="nav-main-link-icon si si-magnifier"></i>
-                      <span class="nav-main-link-name">Search</span>
+                      <span class="nav-main-link-name">Enrolled Courses</span>
                     </a>
                   </li>
                   <li class="nav-main-item">
                     <a class="nav-main-link" href="be_pages_dashboard.html">
                       <i class="nav-main-link-icon si si-action-undo"></i>
-                      <span class="nav-main-link-name">Go Back</span>
+                      <span class="nav-main-link-name">Completed Courses</span>
                     </a>
                   </li>
-                  <li class="nav-main-heading">Extra</li>
-                  <li class="nav-main-item ms-lg-auto">
-                    <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
-                      <i class="nav-main-link-icon fa fa-brush"></i>
-                      <span class="nav-main-link-name d-lg-none">Themes</span>
-                    </a>
-                    <ul class="nav-main-submenu nav-main-submenu-right">
-                      <li class="nav-main-item">
-                        <a class="nav-main-link" data-toggle="theme" data-theme="default" href="#">
-                          <i class="nav-main-link-icon fa fa-square text-default"></i>
-                          <span class="nav-main-link-name">Default</span>
-                        </a>
-                      </li>
-                      <li class="nav-main-item">
-                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/amethyst.min.css" href="#">
-                          <i class="nav-main-link-icon fa fa-square text-amethyst"></i>
-                          <span class="nav-main-link-name">Amethyst</span>
-                        </a>
-                      </li>
-                      <li class="nav-main-item">
-                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/city.min.css" href="#">
-                          <i class="nav-main-link-icon fa fa-square text-city"></i>
-                          <span class="nav-main-link-name">City</span>
-                        </a>
-                      </li>
-                      <li class="nav-main-item">
-                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/flat.min.css" href="#">
-                          <i class="nav-main-link-icon fa fa-square text-flat"></i>
-                          <span class="nav-main-link-name">Flat</span>
-                        </a>
-                      </li>
-                      <li class="nav-main-item">
-                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/modern.min.css" href="#">
-                          <i class="nav-main-link-icon fa fa-square text-modern"></i>
-                          <span class="nav-main-link-name">Modern</span>
-                        </a>
-                      </li>
-                      <li class="nav-main-item">
-                        <a class="nav-main-link" data-toggle="theme" data-theme="assets/css/themes/smooth.min.css" href="#">
-                          <i class="nav-main-link-icon fa fa-square text-smooth"></i>
-                          <span class="nav-main-link-name">Smooth</span>
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
+
                 </ul>
               </div>
               <!-- END Main Navigation -->
