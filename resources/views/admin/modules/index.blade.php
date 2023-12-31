9133545ae3a54e58004d6e06bd25b45487e01aec
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2023-12-31 18:31:54 +0300

    fixed

--- resources/views/admin/modules/index.blade.php
+++ resources/views/admin/modules/index.blade.php
@@ -2,9 +2,14 @@
 
 @section('content')
     <div class="container">
-        <h1 class="mb-4">Modules</h1>
+        <div class="card-header">
+            <h5 class="mb-4">Modules
+                <a href="{{ route('lms.add-module') }}" class="btn btn-primary float-end mb-3">Create Module</a>
 
-        <a href="{{ route('lms.add-module') }}" class="btn btn-primary float-end mb-3">Create Module</a>
+            </h5>
+
+        </div>
+
 
         <div class="card col-12">
             <div class="card-body">
@@ -30,19 +35,19 @@
                                 </td>
                                 <td>{{ $module->created_by }}</td>
                                 <td>
-                                    <a href="{{ route('lms.show-module', $module->id) }}" class="btn btn-sm btn-info">
-                                        <i class="fas fa-eye"></i> View
+                                    <a href="{{ route('lms.show-module', $module->id) }}" class="btn btn-sm btn-secondary">
+                                        <i class="fas fa-eye"></i>
                                     </a>
 
-                                    <a href="{{ route('lms.edit-module', $module->id) }}" class="btn btn-sm btn-warning">
-                                        <i class="fas fa-edit"></i> Edit
+                                    <a href="{{ route('lms.edit-module', $module->id) }}" class="btn btn-sm btn-secondary">
+                                        <i class="fas fa-edit"></i>
                                     </a>
 
                                     <form action="{{ route('lms.delete-module', $module->id) }}" method="post" style="display: inline-block">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">
-                                            <i class="fas fa-trash"></i> Delete
+                                            <i class="fas fa-trash"></i>
                                         </button>
                                     </form>
                                 </td>
