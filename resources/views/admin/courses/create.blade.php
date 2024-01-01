be6ff7dc3be04144f5a670e0ef12654458b9fa99
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-01 20:16:31 +0300

    modified

--- resources/views/admin/courses/create.blade.php
+++ resources/views/admin/courses/create.blade.php
@@ -25,54 +25,60 @@
                     </div>
                     @endif
 
-                    <div class="form-group">
-                        <label for="module_id">Select Module</label>
-                        <select name="module_id" id="module_id" class="form-control" required>
-                            @foreach ($modules as $module)
-                                <option value="{{ $module->id }}">{{ $module->name }}</option>
-                            @endforeach
-                        </select>
+                    <div class="row">
+                        <div class="form-group col-6 mb-2">
+                            <label for="module_id">Select Module</label>
+                            <select name="module_id" id="module_id" class="form-control" required>
+                                @foreach ($modules as $module)
+                                    <option value="{{ $module->id }}">{{ $module->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group col-6 mb-2">
+                            <label for="title">Title</label>
+                            <input type="text" name="title" id="title" class="form-control" required>
+                        </div>
+
+                        <div class="form-group col-6 mb-2">
+                            <label for="title">Slug</label>
+                            <input type="text" name="slug" id="slug" class="form-control" required>
+                        </div>
+
+
+
+                        <div class="form-group col-6 mb-2">
+                            <label for="price">Price</label>
+                            <input type="number" name="price" id="price" class="form-control" required>
+                        </div>
+
+                        <div class="form-group col-6 mb-2">
+                            <label for="duration_in_minutes">Duration (minutes)</label>
+                            <input type="number" name="duration_in_minutes" id="duration_in_minutes" class="form-control" required>
+                        </div>
+
+                        <div class="form-group col-6 mb-2">
+                            <label for="is_published">Published</label>
+                            <select name="is_published" id="is_published" class="form-control">
+                                <option value="1">Yes</option>
+                                <option value="0">No</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-6 mb-2">
+                            <label for="cover_image">Course Cover Image</label>
+                            <input type="file" name="cover_image" id="cover_image" class="form-control-file">
+                        </div>
+
+                        <div class="form-group col-12 mb-2">
+                            <label for="description">Description</label>
+                            <textarea name="description" id="description" class="form-control" rows="4" required></textarea>
+                        </div>
+
                     </div>
 
-                    <div class="form-group">
-                        <label for="title">Title</label>
-                        <input type="text" name="title" id="title" class="form-control" required>
-                    </div>
 
-                    <div class="form-group">
-                        <label for="title">Slug</label>
-                        <input type="text" name="slug" id="slug" class="form-control" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="description">Description</label>
-                        <textarea name="description" id="description" class="form-control" rows="4" required></textarea>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="price">Price</label>
-                        <input type="number" name="price" id="price" class="form-control" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="duration_in_minutes">Duration (minutes)</label>
-                        <input type="number" name="duration_in_minutes" id="duration_in_minutes" class="form-control" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="is_published">Published</label>
-                        <select name="is_published" id="is_published" class="form-control">
-                            <option value="1">Yes</option>
-                            <option value="0">No</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="cover_image">Course Cover Image</label>
-                        <input type="file" name="cover_image" id="cover_image" class="form-control-file">
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">Create Course</button>
+                    <button type="submit" class="btn btn-secondary float-end">Create Course</button>
                 </form>
             </div>
 
