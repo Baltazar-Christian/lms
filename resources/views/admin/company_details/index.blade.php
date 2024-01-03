7d72be553570d1fe31a40d423613257712027986
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-03 20:20:23 +0300

    fixed

--- resources/views/admin/company_details/index.blade.php
+++ resources/views/admin/company_details/index.blade.php
@@ -3,45 +3,58 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container mt-4">
-        <h1>Company Details</h1>
+    <div class="container mt-2">
 
-        <a href="{{ route('company_details.create') }}" class="btn btn-primary float-end mb-3">Add Company Detail</a>
+        <div class="card">
+            <div class="card-header">
+                <h5>
+                    <i class="fa fa-house text-warning"></i>
+                    Company Details
+                    <a href="{{ route('company_details.create') }}" class="btn btn-dark float-end mb-3">Add Company Detail</a>
 
-        @if(session('success'))
-            <div class="alert alert-success">{{ session('success') }}</div>
-        @endif
+                </h5>
+            </div>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse($companyDetails as $companyDetail)
+            <div class="card-body">
+
+                @if(session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>{{ $companyDetail->id }}</td>
-                        <td>{{ $companyDetail->name }}</td>
-                        <td>{{ $companyDetail->description }}</td>
-                        <td>
-                            <a href="{{ route('company_details.edit', $companyDetail->id) }}" class="btn btn-warning">Edit</a>
-                            <form action="{{ route('company_details.destroy', $companyDetail->id) }}" method="post" style="display:inline-block">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                            </form>
-                        </td>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Actions</th>
                     </tr>
-                @empty
-                    <tr>
-                        <td colspan="4">No company details available</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @forelse($companyDetails as $companyDetail)
+                        <tr>
+                            <td>{{ $companyDetail->id }}</td>
+                            <td>{{ $companyDetail->name }}</td>
+                            <td>{{ $companyDetail->description }}</td>
+                            <td>
+                                <a href="{{ route('company_details.edit', $companyDetail->id) }}" class="btn btn-warning">Edit</a>
+                                <form action="{{ route('company_details.destroy', $companyDetail->id) }}" method="post" style="display:inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="4">No company details available</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+            </div>
+        </div>
+
+
     </div>
 @endsection
