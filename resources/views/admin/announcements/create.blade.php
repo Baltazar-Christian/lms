17226722a7c22ea40c7ddd07f6204f776eab1c4c
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-03 12:25:01 +0300

    modified

--- resources/views/admin/announcements/create.blade.php
+++ resources/views/admin/announcements/create.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.master')
 
 @section('content')
+
+<script src="https://cdn.ckeditor.com/ckeditor5/23.0.0/classic/ckeditor.js"></script>
+
     <div class="container mt-4">
 
         <div class="card">
@@ -22,28 +25,35 @@
                     </div>
                 @endif
 
-                    <div class="mb-3">
+                <div class="row">
+
+                    <div class="mb-3 col-6">
                         <label for="title" class="form-label">Title</label>
                         <input type="text" name="title" id="title" class="form-control" required>
                     </div>
 
-                    <div class="mb-3">
-                        <label for="content" class="form-label">Content</label>
-                        <textarea name="content" id="content" class="form-control" rows="4" required></textarea>
-                    </div>
-
-                    <div class="mb-3">
+                    <div class="mb-3 col-6">
                         <label for="attachment" class="form-label">Attachment</label>
                         <input type="file" name="attachment" id="attachment" class="form-control-file">
                     </div>
 
-                    <div class="mb-3">
+                    <div class="mb-3 col-6">
                         <label for="status" class="form-label">Status</label>
                         <select name="status" id="status" class="form-control">
                             <option value="draft">Draft</option>
                             <option value="published">Published</option>
                         </select>
                     </div>
+
+                    <div class="mb-3 col-12">
+                        <label for="content" class="form-label">Content</label>
+                        {{-- <textarea name="content" id="content" class="form-control" rows="4" required></textarea> --}}
+                        <textarea class="form-control" id="content" placeholder="Enter the Content" rows="8" name="content"></textarea>
+
+                    </div>
+                </div>
+
+
                     <hr>
                     <button type="submit" class="btn btn-dark float-end mt-2">Create Announcement</button>
                 </form>
@@ -53,4 +63,15 @@
 
 
     </div>
+    <script>
+        ClassicEditor.create( document.querySelector( '#content' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 @endsection
+
