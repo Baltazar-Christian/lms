f0c3166528ef49f198d67375349c511308fc8d4c
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-07 09:11:08 +0300

    minor

--- resources/views/admin/password/reset.blade.php
+++ resources/views/admin/password/reset.blade.php
@@ -26,9 +26,8 @@
                             <div class="form-group row mb-3">
                                 <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
-                                <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                </div>
+
                             </div>
 
                             <div class="form-group row mb-0">
