d4a56ed71395924c885f41d15de37ff9482ddacd
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-07 10:55:19 +0300

    minor fixes

--- resources/views/tutor/courses/create.blade.php
+++ resources/views/tutor/courses/create.blade.php
@@ -3,15 +3,19 @@
 @extends('layouts.tutor')
 
 @section('content')
-    <div class="container mt-4 mb-2">
+<script src="https://cdn.ckeditor.com/ckeditor5/23.0.0/classic/ckeditor.js"></script>
+
+    <div class="container mt-2 mb-2">
 
 
         <div class="card">
             <div class="card-header">
-                <h5>Create Course
+                <h5>
+                    <i class="fa fa-plus text-warning"></i>
+                    Create Course
 
                     <a href="{{ route('lms.tutor-view-module',$module->id) }}" class="btn btn-dark float-end">
-                        <i class="fa fa-list text-warning"></i>
+                        <i class="fa fa-list "></i>
                         Back
                     </a>
                 </h5>
@@ -35,29 +39,29 @@
                     <div class="row">
                         <input type="hidden" name="module_id" value="{{ $module->id }}" >
 
-                        <div class="form-group col-6 mb-2">
+                        <div class="form-group col-6 mb-3">
                             <label for="title">Title</label>
                             <input type="text" name="title" id="title" class="form-control" required>
                         </div>
 
-                        <div class="form-group col-6 mb-2">
+                        <div class="form-group col-6 mb-3">
                             <label for="title">Slug</label>
                             <input type="text" name="slug" id="slug" class="form-control" required>
                         </div>
 
 
 
-                        <div class="form-group col-6 mb-2">
+                        <div class="form-group col-6 mb-3">
                             <label for="price">Price</label>
                             <input type="number" name="price" id="price" class="form-control" required>
                         </div>
 
-                        <div class="form-group col-6 mb-2">
+                        <div class="form-group col-6 mb-3">
                             <label for="duration_in_minutes">Duration (minutes)</label>
                             <input type="number" name="duration_in_minutes" id="duration_in_minutes" class="form-control" required>
                         </div>
 
-                        <div class="form-group col-6 mb-2">
+                        <div class="form-group col-6 mb-3">
                             <label for="is_published">Published</label>
                             <select name="is_published" id="is_published" class="form-control">
                                 <option value="1">Yes</option>
@@ -65,14 +69,14 @@
                             </select>
                         </div>
 
-                        <div class="form-group col-6 mb-2">
+                        <div class="form-group col-6 mb-3">
                             <label for="cover_image">Course Cover Image</label>
                             <input type="file" name="cover_image" id="cover_image" class="form-control-file">
                         </div>
 
-                        <div class="form-group col-12 mb-2">
+                        <div class="form-group col-12 mb-3">
                             <label for="description">Description</label>
-                            <textarea name="description" id="description" class="form-control" rows="4" required></textarea>
+                            <textarea name="description" id="content" class="form-control" rows="4" required></textarea>
                         </div>
 
                     </div>
@@ -85,4 +89,15 @@
         </div>
 
     </div>
+    <script>
+        ClassicEditor.create( document.querySelector( '#content' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 @endsection
+
--- resources/views/tutor/students/show.blade.php
+++ resources/views/tutor/students/show.blade.php
@@ -2,27 +2,28 @@
 
 @section('content')
 
-<div class="col-12 mt-2 mx-1">
+<div class="container mt-2">
     <div class="card">
         <div class="card-header">
-            <h5>Student Details</h5>
+            <h5>
+                <i class="fa fa-user text-warning"></i>
+                Student Details
+
+
+                    {{-- <a href="{{ route('lms.edit-system-admin', $user->id) }}" class="btn btn-dark btn-sm float-end"><i class="fa fa-edit"></i></a>
+
+                    <form action="{{ route('lms.delete-system-admin', $user->id) }}" class="float-end mx-1" method="POST" style="display: inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm float-end" onclick="return confirm('Are you sure?')"><i class="fa fa-trash"></i></button>
+                    </form> --}}
+                </h5>
         </div>
         <div class="card-body">
             <p><strong>Name:</strong> {{ $user->name }}</p>
             <p><strong>Email:</strong> {{ $user->email }}</p>
 
-            <div class="row">
-                <div class="col-6">
-                    <a href="{{ route('lms.edit-system-admin', $user->id) }}" class="btn btn-warning">Edit</a>
-                </div>
-                <div class="col-6">
-                    <form action="{{ route('lms.delete-system-admin', $user->id) }}" method="POST" style="display: inline-block;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                    </form>
-                </div>
-            </div>
+
 
         </div>
     </div>
