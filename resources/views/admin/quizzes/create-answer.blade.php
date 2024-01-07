1b715f9d62cc7b901833b6a200d974c0a15ca214
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-07 20:28:24 +0300

    fixed

--- resources/views/admin/quizzes/create-answer.blade.php
+++ resources/views/admin/quizzes/create-answer.blade.php
@@ -6,18 +6,21 @@
 
         <div class="card ">
 
-            <h1>Create Answer</h1>
+            <div class="card-header">
+                <h5>Create Answer</h5>
+            </div>
+
 
             <div class="card-body">
                 <form action="{{ route('lms.store-answer', [$course->id, $quiz->id, $question->id]) }}" method="post">
                     @csrf
 
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="text">Answer Text</label>
                         <input type="text" name="answer" id="text" class="form-control" required>
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="is_correct">Is Correct?</label>
                         <select name="is_correct" id="is_correct" class="form-control" required>
                             <option value="1">Yes</option>
@@ -25,7 +28,7 @@
                         </select>
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Add Answer</button>
+                    <button type="submit" class="btn btn-dark float-end">Add Answer</button>
                 </form>
             </div>
         </div>
