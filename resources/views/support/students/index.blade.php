fd97a14a1109c5b5bca0662ed1ebb554ca7f7152
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-10 01:00:56 +0300

    fixed

--- resources/views/support/students/index.blade.php
+++ resources/views/support/students/index.blade.php
@@ -2,13 +2,15 @@
 
 @section('content')
 
-<div class="col-12 mt-2 mx-1">
+<div class="container mt-2 ">
 
     <div class="card">
         <div class="card-header">
             <div class="row">
                 <div class="col-6">
-                    <h5>Students List</h5>
+                    <h5>
+                        <i class="fa fa-users text-warning"></i>
+                        Students List</h5>
                 </div>
                 <div class="col-6">
                     <a href="{{ route('lms.support-add-student') }}" class="btn btn-dark btn-sm float-end">Register</a>
@@ -37,10 +39,10 @@
     <tbody>
         @php
         $i=1;
-        @endp
+        @endphp
         @foreach($users as $user)
             <tr>
-                <td>{{ $user->id }}</td>
+                <td>{{ $i++ }}</td>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
                 <td>
