6a13eca097fb5f1c389487970c8277f4f69c34be
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-10 02:40:08 +0300

    fixed

--- resources/views/support/courses/create.blade.php
+++ resources/views/support/courses/create.blade.php
@@ -2,12 +2,16 @@
 @extends('layouts.support')
 
 @section('content')
-    <div class="container mt-4 mb-2">
+    <div class="container mt-2 mb-2">
 
 
         <div class="card">
             <div class="card-header">
-                <h5>Create Course</h5>
+                <h5> <i class="fa fa-plus text-warning"></i> Create Course
+
+                    <a href="{{ route('lms.support-draft-courses') }}" class="btn btn-dark float-end"> <i class="fa fa-list text-warning"></i> All Courses </a>
+
+                </h5>
             </div>
             <div class="card-body">
                 <form action="{{ route('lms.support-save-course') }}" method="post" enctype="multipart/form-data">
@@ -26,7 +30,7 @@
 
                     <div class="row">
                         <div class="form-group col-6 mb-2">
-                            <label for="module_id">Select Module</label>
+                            <label for="module_id" class="text-dark">Select Module</label>
                             <select name="module_id" id="module_id" class="form-control" required>
                                 @foreach ($modules as $module)
                                     <option value="{{ $module->id }}">{{ $module->name }}</option>
@@ -35,29 +39,29 @@
                         </div>
 
                         <div class="form-group col-6 mb-2">
-                            <label for="title">Title</label>
+                            <label for="title" class="text-dark">Title</label>
                             <input type="text" name="title" id="title" class="form-control" required>
                         </div>
 
                         <div class="form-group col-6 mb-2">
-                            <label for="title">Slug</label>
+                            <label for="title" class="text-dark">Slug</label>
                             <input type="text" name="slug" id="slug" class="form-control" required>
                         </div>
 
 
 
                         <div class="form-group col-6 mb-2">
-                            <label for="price">Price</label>
+                            <label for="price" class="text-dark">Price</label>
                             <input type="number" name="price" id="price" class="form-control" required>
                         </div>
 
                         <div class="form-group col-6 mb-2">
-                            <label for="duration_in_minutes">Duration (minutes)</label>
+                            <label for="duration_in_minutes" class="text-dark">Duration (minutes)</label>
                             <input type="number" name="duration_in_minutes" id="duration_in_minutes" class="form-control" required>
                         </div>
 
                         <div class="form-group col-6 mb-2">
-                            <label for="is_published">Published</label>
+                            <label for="is_published" class="text-dark">Published</label>
                             <select name="is_published" id="is_published" class="form-control">
                                 <option value="1">Yes</option>
                                 <option value="0">No</option>
@@ -65,12 +69,12 @@
                         </div>
 
                         <div class="form-group col-6 mb-2">
-                            <label for="cover_image">Course Cover Image</label>
+                            <label for="cover_image" class="text-dark">Course Cover Image</label>
                             <input type="file" name="cover_image" id="cover_image" class="form-control-file">
                         </div>
 
                         <div class="form-group col-12 mb-2">
-                            <label for="description">Description</label>
+                            <label for="description" class="text-dark">Description</label>
                             <textarea name="description" id="description" class="form-control" rows="4" required></textarea>
                         </div>
 
