e1892df74380875c343f298a79ccbd66b9ef3693
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-11 21:47:38 +0300

    fixed

--- resources/views/tutor/quizzes/show.blade.php
+++ resources/views/tutor/quizzes/show.blade.php
@@ -55,7 +55,7 @@
                                 @foreach ($quiz->questions as $question)
                                     <tr>
                                         <td>{{ $i++ }}</td>
-                                        <td>{{ $question->question }}</td>
+                                        <td width="70%">{!! $question->question !!}</td>
                                         <td>
                                             <a href="{{ route('lms.support-show-question', [$course->id, $quiz->id, $question->id]) }}" class="btn btn-sm btn-dark">
                                                 <i class="fa fa-eye"></i>
