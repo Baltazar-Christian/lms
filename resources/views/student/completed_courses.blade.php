8594596d50fca4b63c58b72bf37af931becf67be
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-13 09:06:12 +0300

    fixed

--- resources/views/student/completed_courses.blade.php
+++ resources/views/student/completed_courses.blade.php
@@ -17,42 +17,135 @@
 
         <!-- Display Enrolled Courses -->
         <div class="row">
-            @forelse($enrolledCourses as $course)
-                <div class="col-md-3 mb-4">
-                    <div class="card">
-                        <a href="{{ route('student-courses.show', $course) }}"> <!-- Updated link -->
 
-                        <img src="{{ asset('public/storage/covers/' . $course->cover_image) }}" width="150px" height="150px" class="card-img-top" alt="{{ $course->name }}">
-                        <div class="card-body">
-                            {{-- <div class="item item-2x item-circle bg-white-10 py-3 my-3 mx-auto">
-                                <!-- ... Existing course icon or image ... -->
-                                <i class="fab fa-html5 fa-2x text-white-75"></i>
-                            </div> --}}
-                            <div class="fs-sm text-white-75">
-                                {{ $course->lessons_count }} lessons &bull; {{ $course->duration }}
+            <div class="col-md-9">
+
+                <!-- Display All Courses -->
+                <div class="row">
+                    @forelse($enrolledCourses as $course)
+                        <div class="col-md-4 mb-4">
+                            <div class="card">
+
+                                <img src="{{ asset('public/storage/covers/' . $course->cover_image) }}" width="150px"
+                                    height="150px" class="card-img-top" alt="{{ $course->name }}">
+                                <div class="card-body">
+
+                                    <h6 class="card-title text-dark">{{ $course->title }}</h5>
+                                        <p class="card-text text-dark">Tsh {{ number_format($course->price, 2) }}</p>
+                                        <!-- Add more course details as needed -->
+
+
+
+                                        <!-- Enroll button -->
+                                        @if (Auth::user()->courses->contains('id', $course->id))
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    @php
+                                                        $enrollment=App\Models\Enrollment::where('user_id',Auth::user()->id)->where('course_id',$course->id)->where('approval_status','approved')->latest()->first();
+                                                    @endphp
+                                                    @if ($enrollment)
+                                                        <a href="{{ route('student-courses.show', $course) }}"
+                                                            class="btn form-control btn-sm  btn-success mb-2">
+                                                            View
+                                                        </a>
+                                                    @else
+                                                        <a href="{{ route('student-unenrolled-courses.show', $course) }}"
+                                                            class="btn form-control btn-sm  btn-success mb-2">
+                                                            View
+                                                        </a>
+                                                    @endif
+
+                                                </div>
+                                                <div class="col-6">
+                                                    <!-- Unenroll button -->
+                                                    <form
+                                                        action="{{ route('students.unenrollSelf', ['student' => Auth::user(), 'course' => $course]) }}"
+                                                        method="post">
+                                                        @csrf
+                                                        {{-- <div class="col-12"> --}}
+                                                        <button type="submit"
+                                                            class=" form-control btn btn-danger btn-sm btn-block">Unenroll</button>
+                                                        {{-- </div> --}}
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        @else
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <a href="{{ route('student-unenrolled-courses.show', $course) }}"
+                                                        class="btn form-control btn-sm  btn-success mb-2">
+                                                        View
+                                                    </a>
+                                                </div>
+                                                <div class="col-md-8">
+                                                    @if ($course->price <= 0)
+                                                        <!-- Enroll button -->
+                                                        <form
+                                                            action="{{ route('students.enrollSelf', ['student' => Auth::user(), 'course' => $course]) }}"
+                                                            method="post">
+                                                            @csrf
+                                                            {{-- <div class="col-12"> --}}
+                                                            <button type="submit"
+                                                                class="btn btn-success btn-sm form-control ">
+                                                                <i class="fa fa-book"></i>
+                                                                Enroll
+                                                            </button>
+                                                            {{-- </div> --}}
+                                                        </form>
+                                                    @else
+                                                        <!-- Enroll button -->
+                                                        <form
+                                                            action="{{ route('students.enrollSelf', ['student' => Auth::user(), 'course' => $course]) }}"
+                                                            method="post">
+                                                            @csrf
+                                                            {{-- <div class="col-12"> --}}
+                                                            <button type="submit"
+                                                                class=" form-control btn btn-warning  btn-sm btn-block">
+
+                                                                <i class="fa fa-shopping-cart"></i>
+                                                                Purchase
+                                                            </button>
+                                                            {{-- </div> --}}
+                                                        </form>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        @endif
+                                </div>
+
                             </div>
                         </div>
-                        <div class="block-content block-content-full">
-                            <h6 class="mb-1 text-dark">{{ $course->title }}</h6>
-                            <p class="text-muted">
-                                <!-- ... Existing course content ... -->
-                                Price: {{ number_format($course->price,2)  }}
-                            </p>
-                            {{-- <div class="fs-sm text-muted">{{ $course->created_at->format('F d, Y') }}</div> --}}
-                            <!-- Unenroll button -->
-                            <form action="{{ route('students.unenrollSelf', ['student' => $user, 'course' => $course]) }}" method="post">
-                                @csrf
-                                <button type="submit" class="btn btn-danger form-control">Unenroll</button>
-                            </form>
+                    @empty
+                        <div class="col-12">
+                            <p>No courses available.</p>
                         </div>
-                        </a>
+                    @endforelse
+                </div>
+            </div>
+
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h5>
+                            <i class="fa fa-list text-warning"></i>
+                            CATEGORIES
+                        </h5>
+                    </div>
+                    <div class="card-body">
+                        @php
+                            $modules = App\Models\Module::get();
+                        @endphp
+                        <ul>
+                            @foreach ($modules as $module)
+                                <li class="nav-link bg-light mt-1"> <a href="{{ route('student-courses.module',$module->id) }}" class="text-dark"> <i
+                                            class="fa fa-book text-warning"></i> <strong> {{ $module->name }}</strong> </a>
+                                </li>
+                            @endforeach
+                        </ul>
+
                     </div>
                 </div>
-            @empty
-                <div class="col-12">
-                    <p>No Completed courses yet.</p>
-                </div>
-            @endforelse
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/student/enrolled_courses.blade.php
+++ resources/views/student/enrolled_courses.blade.php
@@ -16,86 +16,112 @@
         </form>
 
         <div class="row">
+
             <div class="col-md-9">
-                <!-- Display Enrolled Courses -->
+
+                <!-- Display All Courses -->
                 <div class="row">
                     @forelse($enrolledCourses as $course)
                         <div class="col-md-4 mb-4">
                             <div class="card">
-                                <!-- Updated link -->
 
                                 <img src="{{ asset('public/storage/covers/' . $course->cover_image) }}" width="150px"
                                     height="150px" class="card-img-top" alt="{{ $course->name }}">
                                 <div class="card-body">
 
-                                    <div class="fs-sm text-white-75">
-                                        {{ $course->lessons_count }} lessons &bull; {{ $course->duration }}
-                                    </div>
+                                    <h6 class="card-title text-dark">{{ $course->title }}</h5>
+                                        <p class="card-text text-dark">Tsh {{ number_format($course->price, 2) }}</p>
+                                        <!-- Add more course details as needed -->
+
+
+
+                                        <!-- Enroll button -->
+                                        @if (Auth::user()->courses->contains('id', $course->id))
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    @php
+                                                        $enrollment=App\Models\Enrollment::where('user_id',Auth::user()->id)->where('course_id',$course->id)->where('approval_status','approved')->latest()->first();
+                                                    @endphp
+                                                    @if ($enrollment)
+                                                        <a href="{{ route('student-courses.show', $course) }}"
+                                                            class="btn form-control btn-sm  btn-success mb-2">
+                                                            View
+                                                        </a>
+                                                    @else
+                                                        <a href="{{ route('student-unenrolled-courses.show', $course) }}"
+                                                            class="btn form-control btn-sm  btn-success mb-2">
+                                                            View
+                                                        </a>
+                                                    @endif
+
+                                                </div>
+                                                <div class="col-6">
+                                                    <!-- Unenroll button -->
+                                                    <form
+                                                        action="{{ route('students.unenrollSelf', ['student' => Auth::user(), 'course' => $course]) }}"
+                                                        method="post">
+                                                        @csrf
+                                                        {{-- <div class="col-12"> --}}
+                                                        <button type="submit"
+                                                            class=" form-control btn btn-danger btn-sm btn-block">Unenroll</button>
+                                                        {{-- </div> --}}
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        @else
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <a href="{{ route('student-unenrolled-courses.show', $course) }}"
+                                                        class="btn form-control btn-sm  btn-success mb-2">
+                                                        View
+                                                    </a>
+                                                </div>
+                                                <div class="col-md-8">
+                                                    @if ($course->price <= 0)
+                                                        <!-- Enroll button -->
+                                                        <form
+                                                            action="{{ route('students.enrollSelf', ['student' => Auth::user(), 'course' => $course]) }}"
+                                                            method="post">
+                                                            @csrf
+                                                            {{-- <div class="col-12"> --}}
+                                                            <button type="submit"
+                                                                class="btn btn-success btn-sm form-control ">
+                                                                <i class="fa fa-book"></i>
+                                                                Enroll
+                                                            </button>
+                                                            {{-- </div> --}}
+                                                        </form>
+                                                    @else
+                                                        <!-- Enroll button -->
+                                                        <form
+                                                            action="{{ route('students.enrollSelf', ['student' => Auth::user(), 'course' => $course]) }}"
+                                                            method="post">
+                                                            @csrf
+                                                            {{-- <div class="col-12"> --}}
+                                                            <button type="submit"
+                                                                class=" form-control btn btn-warning  btn-sm btn-block">
+
+                                                                <i class="fa fa-shopping-cart"></i>
+                                                                Purchase
+                                                            </button>
+                                                            {{-- </div> --}}
+                                                        </form>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        @endif
                                 </div>
-                                <div class="block-content block-content-full">
-                                    <h6 class="mb-1 text-dark">{{ $course->title }}</h6>
-                                    <p class="text-muted">
-                                        <!-- ... Existing course content ... -->
-                                        Price: {{ number_format($course->price, 2) }}
-                                    </p>
-                                    <!-- resources/views/courses/show.blade.php -->
-                                    @if ($course->approval_status === 'approved')
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <a href="{{ route('student-courses.show', $course) }}"
-                                                    class="btn btn-sm btn-success">
-                                                    View Course
-                                                </a>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <form
-                                                    action="{{ route('students.unenrollSelf', ['student' => $user, 'course' => $course]) }}"
-                                                    method="post">
-                                                    @csrf
-                                                    <button type="submit"
-                                                        class="btn btn-danger form-control">Unenroll</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                        {{-- <span class="badge bg-success">Enrolled</span> --}}
-                                        <!-- Unenroll button -->
-                                    @elseif ($course->approval_status === 'rejected')
-                                        <div class="row">
-                                            <div class="col-md-12">
 
-                                                <span class="badge bg-danger">Enrollment Rejected</span>
-                                            </div>
-                                        </div>
-                                    @else
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <a href="{{ route('student-unenrolled-courses.show', $course) }}"
-                                                    class="btn btn-block btn-sm btn-success">
-                                                    View
-                                                </a>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <span class="badge bg-warning"> Pending </span>
-
-                                            </div>
-                                        </div>
-                                    @endif
-
-                                    {{-- <div class="fs-sm text-muted">{{ $course->created_at->format('F d, Y') }}</div> --}}
-
-                                </div>
-                                </a>
                             </div>
                         </div>
                     @empty
                         <div class="col-12">
-                            <p>No enrolled courses yet.</p>
+                            <p>No courses available.</p>
                         </div>
                     @endforelse
                 </div>
             </div>
 
-       
             <div class="col-md-3">
                 <div class="card">
                     <div class="card-header">
@@ -110,7 +136,7 @@
                         @endphp
                         <ul>
                             @foreach ($modules as $module)
-                                <li class="nav-link bg-light mt-1"> <a href="" class="text-dark"> <i
+                                <li class="nav-link bg-light mt-1"> <a href="{{ route('student-courses.module',$module->id) }}" class="text-dark"> <i
                                             class="fa fa-book text-warning"></i> <strong> {{ $module->name }}</strong> </a>
                                 </li>
                             @endforeach
