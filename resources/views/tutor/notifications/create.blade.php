ae1341220b5aa65beb8d482c60bc5b1364dfe2ef
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-13 07:33:21 +0300

    fixed notifications

--- resources/views/tutor/notifications/create.blade.php
+++ resources/views/tutor/notifications/create.blade.php
@@ -1,25 +1,15 @@
-@extends('layouts.master')
+@extends('layouts.tutor')
 
 @section('content')
-    <h1>Create Notification</h1>
+    <div class="container mt-2">
 
-    @if(session('success'))
-        <div class="alert alert-success" role="alert">
-            {{ session('success') }}
-        </div>
-    @endif
+        <div class="card">
+            <div class="card-header">
+                <h5>Create Notification</h5>
 
-    <form action="{{ route('notifications.store') }}" method="post">
-        @csrf
-
-        <div class="form-group">
-            <label for="course_id">Select Course</label>
-            @extends('layouts.master')
-
-            @section('content')
-                <h1>Create Notification</h1>
-
-                @if(session('success'))
+            </div>
+            <div class="card-body">
+                @if (session('success'))
                     <div class="alert alert-success" role="alert">
                         {{ session('success') }}
                     </div>
@@ -30,3 +20,21 @@
 
                     <div class="form-group">
                         <label for="course_id">Select Course</label>
+                        <select name="course_id" id="course_id" class="form-control" required>
+                            @foreach ($courses as $course)
+                                <option value="{{ $course->id }}">{{ $course->title }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="message">Notification Message</label>
+                        <textarea name="message" id="message" class="form-control" rows="4" required></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Send Notification</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/tutor/notifications/index.blade.php
+++ resources/views/tutor/notifications/index.blade.php
@@ -2,16 +2,23 @@
 @extends('layouts.tutor')
 
 @section('content')
-    <h1>Notifications</h1>
+<div class="container mt-2">
 
-    <table class="table mt-3">
+    <h5>
+        <i class="fa fa-bell text-warning"></i>
+        Notifications
+        <a href="{{ route('notifications.create') }}" class="btn btn-dark float-end">
+            <i class="fa fa-plus"></i>
+            Create New
+        </a>
+    </h5>
+
+    <table class="table table-bordered table-striped table-vcenter js-dataTable-full mt-3">
         <thead>
-            <tr>
                 <th>ID</th>
                 <th>Title</th>
                 <th>Content</th>
                 <th>Actions</th>
-            </tr>
         </thead>
         <tbody>
             @foreach ($notifications as $notification)
@@ -20,16 +27,18 @@
                     <td>{{ $notification->title }}</td>
                     <td>{{ $notification->content }}</td>
                     <td>
-                        <a href="{{ route('notifications.show', $notification->id) }}" class="btn btn-sm btn-info">View</a>
-                        <a href="{{ route('notifications.edit', $notification->id) }}" class="btn btn-sm btn-warning">Edit</a>
+                        <a href="{{ route('notifications.show', $notification->id) }}" class="btn btn-sm btn-dark"><i class="fa fa-eye"></i></a>
+                        <a href="{{ route('notifications.edit', $notification->id) }}" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i></a>
                         <form action="{{ route('notifications.destroy', $notification->id) }}" method="post" style="display: inline-block">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')"><i class="fa fa-trash"></i></button>
                         </form>
                     </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
+</div>
+
 @endsection
