4ff20d29995f0c667efc3f1286f0aced26b63077
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-15 19:20:36 +0300

    fixed show quiz

--- resources/views/student/show_course.blade.php
+++ resources/views/student/show_course.blade.php
@@ -161,39 +161,47 @@
 
                         </div>
                         <div class="card-body">
-                            @forelse ($quizzes as $quiz)
-                                <h6 class="card-title">
-                                    {{-- <i class="fa fa-question text-warning"></i> --}}
-                                    {{ $quiz->title }}
-
+                            <table>
+                                <tbody>
                                     @php
-                                    $enrollment = App\Models\Enrollment::where('user_id', Auth::user()->id)
-                                        ->where('course_id', $course->id)
-                                        ->where('approval_status', 'approved')
-                                        ->latest()
-                                        ->first();
-                                @endphp
-                                @if ($enrollment)
-                                    @if ($quiz->hasUserAttempted(auth()->user()))
-                                        <a href="{{ route('lms.student-show-quiz', ['courseId' => $course->id, 'quizId' => $quiz->id]) }}"
-                                            class="btn btn-sm btn-success float-end ms-2">View Result</a>
-                                    @else
-                                        <a href="{{ route('lms.student-show-quiz', ['courseId' => $course->id, 'quizId' => $quiz->id]) }}"
-                                            class="btn btn-sm btn-warning float-end ms-2">Take Quiz</a>
-                                    @endif
-                                @else
-                                <a href="#" class="btn btn-sm btn-danger float-end disabled">
+                                        $i = 1;
+                                    @endphp
+                                    @forelse ($quizzes as $quiz)
+                                        <tr>
+                                            <td>{{ $i++ }}</td>
+                                            <td width="80%"> {{ $quiz->title }}</td>
+                                            <td>
 
-                                    <i class="fa fa-lock"></i> Take Quiz
-                                </a>
+                                                @php
+                                                    $enrollment = App\Models\Enrollment::where('user_id', Auth::user()->id)
+                                                        ->where('course_id', $course->id)
+                                                        ->where('approval_status', 'approved')
+                                                        ->latest()
+                                                        ->first();
+                                                @endphp
+                                                @if ($enrollment)
+                                                    @if ($quiz->hasUserAttempted(auth()->user()))
+                                                        <a href="{{ route('lms.student-show-quiz', ['courseId' => $course->id, 'quizId' => $quiz->id]) }}"
+                                                            class="btn btn-sm btn-success float-end ms-2">View Result</a>
+                                                    @else
+                                                        <a href="{{ route('lms.student-show-quiz', ['courseId' => $course->id, 'quizId' => $quiz->id]) }}"
+                                                            class="btn btn-sm btn-warning float-end ms-2">Take Quiz</a>
+                                                    @endif
+                                                @else
+                                                    <a href="#" class="btn btn-sm btn-danger float-end disabled">
 
+                                                        <i class="fa fa-lock"></i> Take Quiz
+                                                    </a>
+                                                @endif
+                                            </td>
+                                        </tr>
 
-                                @endif
-                                </h6>
+                                    @empty
+                                        <p class="text-muted">No quizzes available for this course.</p>
+                                    @endforelse
+                                </tbody>
+                            </table>
 
-                            @empty
-                                <p class="text-muted">No quizzes available for this course.</p>
-                            @endforelse
 
                         </div>
                         <!-- End of Quizzes -->
