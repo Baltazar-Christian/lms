09b810f02c20d4a04a5c9b1382670843f625cd7b
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-15 21:42:06 +0300

    fixed

--- resources/views/support/tutors/create.blade.php
+++ resources/views/support/tutors/create.blade.php
@@ -26,26 +26,29 @@
 
             <form action="{{ route('lms.support-save-tutor') }}" method="POST">
                 @csrf
-                <div class="mb-3">
-                    <label for="name" class="form-label text-dark">Name</label>
-                    <input type="text" class="form-control" id="name" name="name" required>
+                <div class="row">
+                    <div class="mb-3 col-6">
+                        <label for="name" class="form-label text-dark">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" required>
+                    </div>
+                    <div class="mb-3 col-6">
+                        <label for="email" class="form-label text-dark">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+                    <div class="mb-3 col-6">
+                        <label for="phone_number" class="form-label text-dark">Phone Number</label>
+                        <input type="tel" class="form-control" id="phone_number" name="phone_number" required>
+                    </div>
+                    <div class="mb-3 col-6">
+                        <label for="address" class="form-label text-dark">Address</label>
+                        <input type="text" class="form-control" id="address" name="address" required>
+                    </div>
+                    <div class="mb-3 col-6">
+                        <label for="password" class="form-label text-dark">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label text-dark">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
-                </div>
-                <div class="mb-3">
-                    <label for="phone_number" class="form-label text-dark">Phone Number</label>
-                    <input type="tel" class="form-control" id="phone_number" name="phone_number" required>
-                </div>
-                <div class="mb-3">
-                    <label for="address" class="form-label text-dark">Address</label>
-                    <input type="text" class="form-control" id="address" name="address" required>
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label text-dark">Password</label>
-                    <input type="password" class="form-control" id="password" name="password" required>
-                </div>
+
                 <hr>
                 <button type="submit" class="btn btn-dark float-end">Save Tutor</button>
             </form>
