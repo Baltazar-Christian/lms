c2dc4b551930e538187d3b6a6532c2db64af26e1
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-16 12:29:42 +0300

    modified student

--- resources/views/support/students/create.blade.php
+++ resources/views/support/students/create.blade.php
@@ -11,36 +11,47 @@
                     Register Student
                     <a href="{{ route('lms.support-students') }}" class="btn btn-dark float-end mt-3">Back </a>
 
-                </h6>
+                    </h6>
             </div>
             <div class="card-body">
-                @if($errors->any())
-                <div class="alert alert-danger mt-3">
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+                @if ($errors->any())
+                    <div class="alert alert-danger mt-3">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
 
-            <form action="{{ route('lms.support-save-student') }}" method="POST">
-                @csrf
-                <div class="mb-3">
-                    <label for="name" class="form-label text-dark">Name</label>
-                    <input type="text" class="form-control" id="name" name="name" required>
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label text-dark">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label text-dark">Password</label>
-                    <input type="password" class="form-control" id="password" name="password" required>
-                </div>
-                <hr>
-                <button type="submit" class="btn btn-dark float-end">Register Student</button>
-            </form>
+                <form action="{{ route('lms.support-save-student') }}" method="POST">
+                    @csrf
+                    <div class="row">
+                        <div class="mb-3 col-6">
+                            <label for="name" class="form-label text-dark">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="email" class="form-label text-dark">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="phone_number" class="form-label text-dark">Phone Number</label>
+                            <input type="tel" class="form-control" id="phone_number" name="phone_number" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="address" class="form-label text-dark">Address</label>
+                            <input type="text" class="form-control" id="address" name="address" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="password" class="form-label text-dark">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+                    </div>
+
+                    <hr>
+                    <button type="submit" class="btn btn-dark float-end">Register Student</button>
+                </form>
             </div>
 
         </div>
--- resources/views/support/tutors/edit.blade.php
+++ resources/views/support/tutors/edit.blade.php
@@ -2,61 +2,65 @@
 
 @section('content')
 
-<div class="container mt-2">
-    <div class="card">
-        <div class="card-header">
-            <h5>Edit Tutor
+    <div class="container mt-2">
+        <div class="card">
+            <div class="card-header">
+                <h5>Edit Tutor
 
-                <a href="{{ route('lms.support-tutors') }}" class="btn btn-dark float-end mt-3">Back </a>
+                    <a href="{{ route('lms.support-tutors') }}" class="btn btn-dark float-end mt-3">Back </a>
 
-            </h5>
-        </div>
+                </h5>
+            </div>
 
-        <div class="card-body">
-            @if($errors->any())
-            <div class="alert alert-danger mt-3">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+            <div class="card-body">
+                @if ($errors->any())
+                    <div class="alert alert-danger mt-3">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
 
-        <form action="{{ route('lms.support-update-tutor', $user->id) }}" method="POST">
-            @csrf
-            @method('PUT')
-            <div class="row">
-            <div class="mb-3 col-6">
-                <label for="name" class="form-label text-dark">Name</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ $user->name }}" required>
+                <form action="{{ route('lms.support-update-tutor', $user->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class="row">
+                        <div class="mb-3 col-6">
+                            <label for="name" class="form-label text-dark">Name</label>
+                            <input type="text" class="form-control" id="name" name="name"
+                                value="{{ $user->name }}" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="email" class="form-label text-dark">Email</label>
+                            <input type="email" class="form-control" id="email" name="email"
+                                value="{{ $user->email }}" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="phone_number" class="form-label text-dark">Phone Number</label>
+                            <input type="tel" class="form-control" id="phone_number" value="{{ $user->phone_number }}"
+                                name="phone_number" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="address" class="form-label text-dark">Address</label>
+                            <input type="text" class="form-control" id="address" value="{{ $user->address }}"
+                                name="address" required>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="password" class="form-label text-dark">Role</label>
+                            <select name="role" class="form-control" id="">
+                                <option value="student" {{ $user->role === 'student' ? 'selected' : '' }}>Student</option>
+                                <option value="tutor" {{ $user->role === 'tutor' ? 'selected' : '' }}>Tutor</option>
+                                <option value="support" {{ $user->role === 'support' ? 'selected' : '' }}>Admin</option>
+                            </select>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-dark float-end">Update </button>
+                </form>
             </div>
-            <div class="mb-3 col-6">
-                <label for="email" class="form-label text-dark">Email</label>
-                <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" required>
-            </div>
-            <div class="mb-3 col-6">
-                <label for="phone_number" class="form-label text-dark">Phone Number</label>
-                <input type="tel" class="form-control" id="phone_number" value="{{ $user->phone_number }}"  name="phone_number" required>
-            </div>
-            <div class="mb-3 col-6">
-                <label for="address" class="form-label text-dark">Address</label>
-                <input type="text" class="form-control" id="address" value="{{ $user->address }}" name="address" required>
-            </div>
-            <div class="mb-3 col-6">
-                <label for="password" class="form-label text-dark">Role</label>
-                <select name="role" class="form-control" id="">
-                    <option value="student" {{ $user->role==='student' ? 'selected' : '' }}>Student</option>
-                    <option value="tutor" {{ $user->role==='tutor' ? 'selected' : '' }}>Tutor</option>
-                    <option value="support" {{ $user->role==='support' ? 'selected' : '' }}>Admin</option>
-                </select>
-            </div>
-            </div>
-            <button type="submit" class="btn btn-dark float-end">Update </button>
-        </form>
         </div>
     </div>
-</div>
 
 
 
