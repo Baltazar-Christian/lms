ad410f3298ddb0df2d011d11e94973b19309f407
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-16 14:06:50 +0300

    fixed

--- resources/views/support/courses/show.blade.php
+++ resources/views/support/courses/show.blade.php
@@ -53,7 +53,7 @@
                                 <button class=" btn btn-dark btn-sm" id="btabs-static2-settings-tab" data-bs-toggle="tab"
                                     data-bs-target="#btabs-static2-settings" role="tab"
                                     aria-controls="btabs-static2-settings" aria-selected="false">
-                                    <i class="si si-user text-warning"></i>
+                                    <i class="fa fa-user text-warning"></i>
                                     <span class="">Students</span>
                                 </button>
                             </li>
