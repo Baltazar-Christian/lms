33dd034cf7177ee7f294c794d3f176135970c907
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-16 13:01:47 +0300

    fixed

--- app/Http/Controllers/support/SupportUserManagementController.php
+++ app/Http/Controllers/support/SupportUserManagementController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\support;
 
 use App\Models\User;
+use App\Models\Course;
 use App\Models\QuizResult;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
