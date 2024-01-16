d696743916a9e402efd96031a695793f6b6f0806
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-16 13:10:06 +0300

    fixed

--- resources/views/support/tutors/show.blade.php
+++ resources/views/support/tutors/show.blade.php
@@ -5,17 +5,11 @@
 <div class="col-12 mt-2 mx-1">
     <div class="card">
         <div class="card-header">
-            <h5> <i class="fa fa-user text-warning"></i> Tutor Details
+            <h5> <i class="fa fa-user text-warning"></i>  Details
                 {{-- <div class="col-6"> --}}
                     <a href="{{ route('lms.support-edit-tutor', $user->id) }}" class="btn btn-sm btn-dark float-end"><i class="fa fa-edit"></i></a>
-                {{-- </div>
-                <div class="col-6"> --}}
-                    <form action="{{ route('lms.support-delete-tutor', $user->id) }}" method="POST" class="float-end m-1" style="display: inline-block;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm float-end" onclick="return confirm('Are you sure?')"><i class="fa fa-trash"></i></button>
-                    </form>
-                {{-- </div> --}}
+
+
             </h5>
         </div>
         <div class="card-body">
