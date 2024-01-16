6c075152f44ae1b0f2f56e22aa67c4e2a86d04ce
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-16 23:56:31 +0300

    fixed

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -2,19 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Course;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Enrollment extends Model
 {
     use HasFactory;
 
 
-    protected $fillable = ['student_id', 'course_id','approval_status'];
+    protected $fillable = ['student_id', 'course_id', 'approval_status'];
 
     public function courses()
     {
         return $this->belongsToMany(Course::class, 'course_id');
     }
 
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'course_id', 'id');
+    }
 }
