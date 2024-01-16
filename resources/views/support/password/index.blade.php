4335a291e491475ae39011f741698ab8e8886694
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-16 10:38:03 +0300

    modified

--- resources/views/support/password/index.blade.php
+++ resources/views/support/password/index.blade.php
@@ -1,60 +1,61 @@
 @extends('layouts.support')
 
 @section('content')
+    <div class="col-12 mt-2 mb-2">
 
-<div class="col-12 mt-2 mb-2">
-
-    <div class="card">
-        <div class="card-header">
-            <div class="row">
-                <div class="col-6">
-                    <h5><i class="fa fa-users text-warning"></i> Users List</h5>
-                </div>
-                <div class="col-6">
-                    {{-- <a href="{{ route('lms.add-tutor') }}" class="btn btn-success float-end">Register Tutor</a> --}}
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-6">
+                        <h5><i class="fa fa-users text-warning"></i> Users List</h5>
+                    </div>
+                    <div class="col-6">
+                        {{-- <a href="{{ route('lms.add-tutor') }}" class="btn btn-success float-end">Register Tutor</a> --}}
+                    </div>
                 </div>
             </div>
+
+            <div class="card-body">
+
+                @if (session('success'))
+                    <div class="alert alert-success mt-3">
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped table-vcenter js-dataTable-responsive">
+                        <thead>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Actions</th>
+                        </thead>
+                        <tbody>
+                            @php
+                                $i = 1;
+                            @endphp
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td>{{ $i++ }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>
+                                        <a href="{{ route('support.password.reset', $user->id) }}"
+                                            class="btn btn-dark btn-sm">
+                                            <i class="fa fa-key"></i>
+                                            Reset Password
+                                        </a>
+
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
         </div>
-
-        <div class="card-body">
-
-    @if(session('success'))
-    <div class="alert alert-success mt-3">
-        {{ session('success') }}
     </div>
-@endif
-
-<table class="table table-bordered table-striped table-vcenter js-dataTable-responsive">
-    <thead>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Actions</th>
-    </thead>
-    <tbody>
-        @php
-        $i=1;
-        @endphp
-        @foreach($users as $user)
-            <tr>
-                <td>{{ $i++ }}</td>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>
-                    <a href="{{ route('support.password.reset', $user->id) }}" class="btn btn-dark btn-sm">
-                        <i class="fa fa-key"></i>
-                        Reset Password
-                    </a>
-
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
-        </div>
-
-    </div>
-</div>
-
-
 @endsection
--- resources/views/support/tutors/show.blade.php
+++ resources/views/support/tutors/show.blade.php
@@ -21,12 +21,16 @@
         <div class="card-body">
             <p class="text-dark"><strong>Name:</strong> {{ $user->name }}</p>
             <p class="text-dark" ><strong>Email:</strong> {{ $user->email }}</p>
+            <p class="text-dark" ><strong>Phone Number:</strong> {{ $user->phone_number }}</p>
+            <p class="text-dark" ><strong>Address:</strong> {{ $user->phone_number }}</p>
+            <p class="text-dark" ><strong>Joined:</strong> {{ $user->created_at }}</p>
 
             <div class="row">
                 <h5><i class="fa fa-book text-warning"></i> Created Courses</h5>
                 <table class="table table-bordered table-striped table-vcenter js-dataTable-responsive">
 
                     <thead hidden>
+                        <th></th>
                         <th></th>
                         <th></th>
                         <th></th>
@@ -43,6 +47,11 @@
                             <td>
                                 <span class="fs-xs fw-semibold d-inline-block py-1 px-3 rounded-pill   {{  $course->is_published ? 'bg-success-light text-success' : 'bg-danger-light text-danger' }} ">
                                     {{  $course->is_published?'Published':'Not Published' }}</span>
+                            </td>
+                            <td>
+                                <a href="{{ route('lms.support-show-course', $course->id) }}" class="btn btn-sm btn-dark">
+                                    <i class="fa fa-eye"></i>
+                                    </a>
                             </td>
 
                         </tr>
