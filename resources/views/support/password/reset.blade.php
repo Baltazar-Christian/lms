052408e609cbb6378cf5bd2b99a4b5cb1fc53898
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-17 15:33:45 +0300

    fixed reset password

--- resources/views/support/password/reset.blade.php
+++ resources/views/support/password/reset.blade.php
@@ -7,8 +7,8 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">  
-                        
+                    <div class="card-header">
+
                         <h5> <i class="fa fa-key text-warning"></i> {{ $user->name }} {{ __('Reset Password') }}</h5>
                     </div>
 
@@ -17,8 +17,25 @@
                             @csrf
 
 
+                            <div class="mb-4 col-12">
+                                <div class="input-group">
+                                    <input type="password" class="form-control form-control-lg form-control-alt" id="signup-password" name="password" placeholder="Password">
+                                    <button class="btn btn btn-alt-info" type="button" id="toggle-signup-password">
+                                        <i class="fas fa-eye"></i>
+                                    </button>
+                                </div>
+                            </div>
 
-                            <div class="form-group row mb-2">
+                            <div class="mb-4 col-12">
+                                <div class="input-group">
+                                    <input type="password" class="form-control form-control-lg form-control-alt" id="signup-password-confirm" name="password_confirmation" placeholder="Confirm Password">
+                                    <button class="btn btn btn-alt-info" type="button" id="toggle-signup-password-confirm">
+                                        <i class="fas fa-eye"></i>
+                                    </button>
+                                </div>
+                            </div>
+
+                            {{-- <div class="form-group row mb-2">
                                 <label for="password" class="col-md-4 col-form-label text-dark text-md-right">{{ __('Password') }}</label>
 
                                     <input id="password" type="password" class="form-control" name="password" required autocomplete="new-password">
@@ -29,7 +46,7 @@
 
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
 
-                            </div>
+                            </div> --}}
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-6"></div>
@@ -45,4 +62,32 @@
             </div>
         </div>
     </div>
+
+
+    <script src="{{ asset('assets/js/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            function togglePasswordVisibility(passwordInput, toggleButton) {
+                const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+                passwordInput.setAttribute('type', type);
+                toggleButton.querySelector('i').classList.toggle('fa-eye-slash');
+            }
+
+            const signupPasswordInput = document.getElementById('signup-password');
+            const toggleSignupPasswordButton = document.getElementById('toggle-signup-password');
+
+            toggleSignupPasswordButton.addEventListener('click', function () {
+                togglePasswordVisibility(signupPasswordInput, this);
+            });
+
+            const signupPasswordConfirmInput = document.getElementById('signup-password-confirm');
+            const toggleSignupPasswordConfirmButton = document.getElementById('toggle-signup-password-confirm');
+
+            toggleSignupPasswordConfirmButton.addEventListener('click', function () {
+                togglePasswordVisibility(signupPasswordConfirmInput, this);
+            });
+        });
+    </script>
 @endsection
