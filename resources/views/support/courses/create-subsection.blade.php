d6a887a89799203ed142b367b92b20b78e990067
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-17 05:07:17 +0300

    fixed

--- resources/views/support/courses/create-subsection.blade.php
+++ resources/views/support/courses/create-subsection.blade.php
@@ -1,88 +1,101 @@
-
 @extends('layouts.support')
 
 @section('content')
-<script src="https://cdn.ckeditor.com/ckeditor5/23.0.0/classic/ckeditor.js"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/23.0.0/classic/ckeditor.js"></script>
 
-<div class="container mt-2">
-    <div class="card">
-        <div class="card-header">
-            <h5>Create Sub-Section</h5>
-        </div>
-        <div class="card-body">
-            <form action="{{ route('lms.support-save-subsection', ['courseId' => $course->id, 'parentId' => $parentContent->id]) }}" method="post" enctype="multipart/form-data">
-                @csrf
+    <div class="container mt-2">
+        <div class="card">
+            <div class="card-header">
+                <h5>Create Sub-Section
 
-                {{-- Include validation errors if any --}}
-                @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-                @endif
-                <div class="row">
-                    <div class="form-group mb-3 col-6">
-                        <label for="title">Title</label>
-                        <input type="text" name="title" id="title" class="form-control" required>
-                    </div>
+                    <a href="{{ route('lms.support-show-course-content', ['courseId' => $course->id, 'contentId' => $parentContent->id]) }}"
+                        class="btn btn-sm btn-dark float-end">
+                        <i class="fa fa-list"></i> Back
+                    </a>
+                </h5>
+            </div>
+            <div class="card-body">
+                <form
+                    action="{{ route('lms.support-save-subsection', ['courseId' => $course->id, 'parentId' => $parentContent->id]) }}"
+                    method="post" enctype="multipart/form-data">
+                    @csrf
 
-                    <div class="form-group mb-3 col-6">
-                        <label for="type">Content Type</label>
-                        <select name="type" id="type" class="form-control" required>
-                            <option value="text">Text</option>
-                            <option value="pdf">PDF</option>
-                            <option value="image">Image</option>
-                            <option value="video">Video</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group mb-3 col-6">
-                        <label for="file">File</label>
-                        <span class="text-muted fs-sm">
-                            Please upload an file with the following conditions:
+                    {{-- Include validation errors if any --}}
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
                             <ul>
-                                <li>'pdf'</li>
-                                <li>'image' (Accepted formats: jpg, png, webp)</li>
-                                <li>'video' (Accepted formats: mp4)</li>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
                             </ul>
-                        </span>
-                        <input type="file" name="file" id="file" class="form-control-file">
+                        </div>
+                    @endif
+                    <div class="row">
+                        <div class="form-group mb-3 col-6">
+                            <label for="title">Title</label>
+                            <input type="text" name="title" id="title" class="form-control" required>
+                        </div>
+
+                        <div class="form-group mb-3 col-6">
+                            <label for="type">Content Type</label>
+                            <select name="type" id="type" class="form-control" required>
+                                <option value="text">Text</option>
+                                <option value="pdf">PDF</option>
+                                <option value="image">Image</option>
+                                <option value="video">Video</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group mb-3 col-6">
+                            <label for="file">File</label>
+                            <span class="text-muted fs-sm">
+                                Please upload an file with the following conditions:
+                                <ul>
+                                    <li>'pdf'</li>
+                                    <li>'image' (Accepted formats: jpg, png, webp)</li>
+                                    <li>'video' (Accepted formats: mp4)</li>
+                                </ul>
+                            </span>
+                            <input type="file" name="file" id="file" class="form-control-file">
+                        </div>
+
+                        <div class="form-group mb-3 col-6">
+                            <label for="duration">Duration (minutes)</label>
+                            <input type="number" name="duration" id="duration" class="form-control">
+                        </div>
+                        <div class="form-group mb-3 col-6">
+                            <label for="title">Video Link <span class="text-muted">For Youtube Videos</span> </label>
+                            <input type="text" name="url" id="title" class="form-control">
+                        </div>
                     </div>
 
-                    <div class="form-group mb-3 col-6">
-                        <label for="duration">Duration (minutes)</label>
-                        <input type="number" name="duration" id="duration" class="form-control">
+
+                    <div class="form-group mb-3">
+                        <label for="description">Description</label>
+                        <textarea class="form-control" id="description" placeholder="Enter the Description" rows="8" name="description"></textarea>
                     </div>
-                    <div class="form-group mb-3 col-6">
-                        <label for="title">Video Link <span class="text-muted">For Youtube Videos</span> </label>
-                        <input type="text" name="url" id="title" class="form-control" >
-                    </div>
-                </div>
 
 
-                <div class="form-group mb-3">
-                    <label for="description">Description</label>
-                    <textarea class="form-control" id="description" placeholder="Enter the Description" rows="8" name="description"></textarea>
-                </div>
-
-
-                <button type="submit" class="btn btn-dark float-end">Create Sub-Section</button>
-            </form>
+                    <button type="submit" class="btn btn-dark float-end">Create Sub-Section</button>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
-<script>
-    ClassicEditor.create( document.querySelector( '#description' ) )
-        .catch( error => {
-            console.error( error );
-        } );
-</script>
+    <script>
+        ClassicEditor.create(document.querySelector('#description'))
+            .catch(error => {
+                console.error(error);
+            });
+    </script>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 @endsection
