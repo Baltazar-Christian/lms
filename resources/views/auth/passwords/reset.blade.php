a899238acbb946c506d0d4f8bb0a162f70029db3
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-17 04:39:12 +0300

    fixed

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,193 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+    <title>UPPER - Learn, Grow, Succeed</title>
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+    <meta name="description"
+        content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+    <meta name="author" content="pixelcave">
+    <meta name="robots" content="noindex, nofollow">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+    <!-- Open Graph Meta -->
+    <meta property="og:title" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework">
+    <meta property="og:site_name" content="OneUI">
+    <meta property="og:description"
+        content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email ?? old('email') }}" required autocomplete="email" autofocus>
+    <!-- Icons -->
+    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+    <link rel="shortcut icon" href="assets/media/favicons/favicon.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">
+    <!-- END Icons -->
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- Stylesheets -->
+    <!-- OneUI framework -->
+    <link rel="stylesheet" id="css-main" href="assets/css/oneui.min.css">
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+    <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+    <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/amethyst.min.css"> -->
+    <!-- END Stylesheets -->
+</head>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<body class="bg-secondary">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <i class="fa fa-graduation-cap text-warning"></i>
+                UPPER</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item m-1">
+                        <a class=" btn btn-info" href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li class="nav-item m-1">
+                        <a class="btn btn-warning  " href="{{ route('register') }}">Register</a>
+                    </li>
+                </ul>
             </div>
         </div>
+    </nav>
+
+
+    <div id="page-container">
+
+        <!-- Main Container -->
+        <main id="main-container">
+            <!-- Page Content -->
+            <div class="hero-static d-flex align-items-center">
+                <div class="content">
+                    <div class="row justify-content-center push">
+                        <div class="col-md-8 col-lg-8 col-xl-8 col-12">
+                            <!-- Sign In Block -->
+                            <div class="block block-rounded mb-0">
+                                <div class="block-header block-header-default">
+                                    {{-- <h3 class="block-title">Sign In</h3> --}}
+                                    <div class="block-options">
+                                        {{-- <a class="btn-block-option fs-sm" href="op_auth_reminder.html">Forgot Password?</a> --}}
+                                        {{-- <a class="btn-block-option" href="op_auth_signup.html" data-bs-toggle="tooltip" data-bs-placement="left" title="New Account">
+                        <i class="fa fa-user-plus"></i>
+                      </a> --}}
+                                    </div>
+                                </div>
+                                <div class="block-content">
+                                    <div class="p-sm-3 px-lg-4 px-xxl-5 py-lg-5">
+                                        <h1 class="h2 mb-1 text-center">
+                                            <i class="nav-main-link-icon fa fa-graduation-cap text-warning"></i>
+                                            UPPER
+                                        </h1>
+                                        {{-- <p class="fw-medium text-muted text-center ">
+                                            Forgot your password? No worries! Enter your email below, and
+                                            we'll send you a reset link.
+                                        </p> --}}
+                                        <form method="POST" action="{{ route('password.update') }}">
+                                            @csrf
+
+                                            <input type="hidden" name="token" value="{{ $token }}">
+
+                                            <div class="row mb-3" hidden>
+                                                <label for="email"
+                                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="email" type="email"
+                                                        class="form-control @error('email') is-invalid @enderror"
+                                                        name="email" value="{{ $user->email ?? old('email') }}"
+                                                        required autocomplete="email" autofocus>
+
+                                                    @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="row mb-3">
+                                                <label for="password"
+                                                    class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="password" type="password"
+                                                        class="form-control @error('password') is-invalid @enderror"
+                                                        name="password" required autocomplete="new-password">
+
+                                                    @error('password')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="row mb-3">
+                                                <label for="password-confirm"
+                                                    class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                                                <div class="col-md-6">
+                                                    <input id="password-confirm" type="password" class="form-control"
+                                                        name="password_confirmation" required
+                                                        autocomplete="new-password">
+                                                </div>
+                                            </div>
+
+                                            <div class="row mb-0">
+                                                <div class="col-md-6 offset-md-4">
+                                                    <button type="submit" class="btn btn-primary">
+                                                        {{ __('Reset Password') }}
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <!-- END Sign In Form -->
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- END Sign In Block -->
+                        </div>
+                    </div>
+                    <div class="fs-sm text-muted text-center">
+                        <strong class="text-warning">UPPER</strong> &copy; <span data-toggle="year-copy"></span>
+                    </div>
+                </div>
+            </div>
+            <!-- END Page Content -->
+        </main>
+        <!-- END Main Container -->
     </div>
-</div>
-@endsection
+    <!-- END Page Container -->
+
+    <!--
+OneUI JS
+
+Core libraries and functionality
+webpack is putting everything together at assets/_js/main/app.js
+-->
+    <script src="{{ asset('assets/js/oneui.app.min.js') }}"></script>
+
+    <!-- jQuery (required for jQuery Validation plugin) -->
+    <script src="{{ asset('assets/js/lib/jquery.min.js') }}"></script>
+
+    <!-- Page JS Plugins -->
+    <script src="{{ asset('assets/js/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+
+    <!-- Page JS Code -->
+    <script src="{{ asset('assets/js/pages/op_auth_signin.min.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
