37ef31de596881a7a0b5fcf7a1d5a1c2e78b3e2b
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-17 18:22:46 +0300

    fixed

--- resources/views/tutor/notifications/show.blade.php
+++ resources/views/tutor/notifications/show.blade.php
@@ -3,14 +3,26 @@
 @extends('layouts.tutor')
 
 @section('content')
-    <h1>Notification</h1>
+
+<div class="container">
 
     <div class="card mt-3">
+        <div class="card-header">
+
+            <h5>
+                <i class="fa fa-bell text-warning"></i>
+                Notification
+                <a href="{{ route('notifications.index') }}" class="btn btn-dark float-end mt-3"> <i class="fa fa-list"></i> Back </a>
+
+            </h5>
+
+        </div>
         <div class="card-body">
-            <h3 class="card-title">{{ $notification->title }}</h3>
-            <p class="card-text">{{ $notification->content }}</p>
+            <h3 class="card-title">{{ $notification->course }}</h3>
+            <p class="card-text">{!! $notification->message!!}</p>
         </div>
     </div>
 
-    <a href="{{ route('notifications.index') }}" class="btn btn-primary mt-3">Back to Notifications</a>
+</div>
+
 @endsection
