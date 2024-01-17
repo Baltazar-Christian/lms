e938c565c6e1ae12749f3ce6a00eca75f453c74d
Baltazar Christian (Tazar Chriss) <120463084+Baltazar-Christian@users.noreply.github.com>
2024-01-17 09:55:57 +0300

    modified

---
+++ resources/views/student/all_notifications.blade.php
@@ -0,0 +1,51 @@
+
+
+<!-- resources/views/quizzes/result.blade.php -->
+@extends('layouts.student')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h5> <i class="fa fa-bell text-warning"></i> All Notifications</h5>
+
+            </div>
+            <div class="card-body">
+                <table>
+                    <thead>
+                        <th>SN</th>
+                        <th>Date</th>
+                        <th>Course</th>
+                        <th>Status</th>
+                        <th>Option</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($notifications as $notification )
+                        <tr>
+                            <td>{{ $i++ }}</td>
+                            <td>{{  $notification->created_at->format('d/m/Y') }}</td>
+                            <td>{{  $notification->course->title }}</td>
+                            <td>
+
+                                @php
+                                    $read=App\Models\UserNotification::where('user_id',Auth::user()->id)->where('notification_id',$notification->id)->first();
+
+                                @endphp
+
+                                @if ($read)
+                                    <span class="text-success">Read</span>
+                                @else
+                                <span class="text-warning">New</span>
+
+                                @endif
+                            </td>
+                            <td>Option</td>
+                        </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
